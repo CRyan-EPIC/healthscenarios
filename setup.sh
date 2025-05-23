ba30484657784e01daa8ace65a28691105bfe2ff
Cameron Ryan <125604130+CRyan-EPIC@users.noreply.github.com>
2025-05-23 12:04:26 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,13 @@ sudo apt install tmux curl git wget python3-venv kitty -y
 curl -fsSL https://ollama.com/install.sh | sh
 cd ~/healthscenarios
 
+#add cron job
 echo "* * * * * cd /home/pi/healthscenarios && /usr/bin/git pull origin main"
+
+#download kitty
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
+
+#In case we need local llms
 ollama pull llama3.2:3b
 ollama pull gemma3:4b
