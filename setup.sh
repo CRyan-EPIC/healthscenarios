7c9e95561f88e08b791808f7b0877d3b8d69426b
Cameron Ryan <125604130+CRyan-EPIC@users.noreply.github.com>
2025-05-23 13:48:37 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install tmux curl git wget python3-venv kitty mes-utils -y
+sudo apt install tmux curl git wget python3-venv mes-utils sudo apt install fonts-noto-color-emoji -y
 
 #Install ollama
 curl -fsSL https://ollama.com/install.sh | sh
@@ -11,8 +11,8 @@ echo "* * * * * cd /home/pi/healthscenarios && /usr/bin/git pull origin main"
 echo "
 
 #download kitty
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
+#curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+#ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
 
 #In case we need local llms
 ollama pull llama3.2:3b
