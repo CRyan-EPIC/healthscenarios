f5c7e3980cc14e4df6a6dbdf2fcda89bfc419d33
Cameron Ryan <125604130+CRyan-EPIC@users.noreply.github.com>
2025-05-23 13:55:39 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,10 +10,5 @@ cd ~/healthscenarios
 ( crontab -l 2>/dev/null; echo "* * * * * cd /home/pi/healthscenarios && /usr/bin/git pull origin main" ) | crontab -
 ( sudo crontab -l 2>/dev/null; echo '45 11 * * 1-5 /usr/sbin/shutdown -h now' ) | sudo crontab -
 
-#download kitty
-#curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-#ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
-
 #In case we need local llms
 ollama pull llama3.2:3b
-ollama pull gemma3:4b
